ee0a30d435eec98b3ce0b88bab8248e7503aa3a7
Zachary Conger <zachary.conger@stackhawk.com>
2022-01-02 20:47:55 -0700

    Added a post function, but forgot to make sure we can't post the same UUID twice.

--- src/main/kotlin/com/zconger/sburcoffee/RestApiDemoController.kt
+++ src/main/kotlin/com/zconger/sburcoffee/RestApiDemoController.kt
@@ -4,20 +4,29 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 class RestApiDemoController {
-//    init {
-        var coffees: List<Coffee> = listOf(
-            Coffee("Café Cereza"),
-            Coffee("Café Ganador"),
-            Coffee("Café Lareño"),
-            Coffee("Café Três Pontas")
-        )
-//    }
+    var coffees: MutableList<Coffee> = mutableListOf(
+        Coffee("Café Cereza"),
+        Coffee("Café Ganador"),
+        Coffee("Café Lareño"),
+        Coffee("Café Três Pontas")
+    )
 
     @GetMapping(value = ["/coffees"])
-    fun coffees(): List<Coffee> = coffees
+    fun coffees(): MutableList<Coffee> = coffees
 
     @GetMapping(value = ["/coffees/{id}"])
-    fun coffeeBtId(@PathVariable id: String): List<Coffee>? {
+    fun coffeeById(@PathVariable id: String): List<Coffee> {
         return coffees.filter { it.id == id }
     }
+
+    @PostMapping(value = ["/coffees"])
+    fun coffeePost(@RequestBody coffee: Coffee): Coffee {
+        coffees.add(coffee)
+        return coffee
+    }
+
+//    @PutMapping(value = ["/coffees/{id}"])
+//    fun coffeePut(@PathVariable id: String, @RequestBody coffee: Coffee): Coffee {
+//
+//    }
 }
